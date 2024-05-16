926f1a946783ac75100e90e39c2cb139111ad401
saifil <saifilmomin4@gmail.com>
2024-05-16 18:50:39 -0400

    fix in doc

--- src/sections/GettingStarted.tsx
+++ src/sections/GettingStarted.tsx
@@ -11,14 +11,14 @@ const GettingStarted: React.FC = () => {
             <Heading as="h1">Getting Started</Heading>
             <br />
             <Text>
-            Reactive Charts is an collection of examples for visualizations, created using React, D3 and Framer motion. The aim of this project is to create a declarative approach to create D3 charts.
+                Reactive Charts is a compilation of visualization examples built with React, D3, and Framer Motion. The project aims to provide a declarative method for creating D3 charts.
             </Text>
             <br />
             <Heading fontSize="24px" as="h2">Installation</Heading>
             <br />
             <CodeBlock code={`npm install d3 @types/d3 framer-motion`} />
             <br />
-            <br />  
+            <br />
             {/* <Heading fontSize="24px" as="h2">Usage</Heading>
             <br />
             <Text>
