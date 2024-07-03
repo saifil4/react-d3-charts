0a65208ad82d85fa25ad0200d1c2e22d3b8c7221
saifil <saifilmomin4@gmail.com>
2024-07-03 13:05:54 -0400

    added documentation to getting started

--- src/sections/GettingStarted.tsx
+++ src/sections/GettingStarted.tsx
@@ -19,6 +19,16 @@ const GettingStarted: React.FC = () => {
             <CodeBlock code={`npm install d3 @types/d3 framer-motion`} />
             <br />
             <br />
+            <Heading fontSize="24px" as="h2">Why</Heading><br />
+            <Text>
+                I came across d3 rougly 5 years ago and I used it to create some really amazing charts. However I made lot of mistakes of implementing it the wrong way in React. I read some articles and wacthed some videos on how to use d3 with react and I was able to create some amazing charts. However I felt that the code was not very declarative and it was very hard to understand. So
+                I wanted to create a something that helps us create very amazing delcarative code for creating charts using d3
+            </Text>
+            <br />
+            <Heading fontSize="24px" as="h2">How to use?</Heading><br />
+            <Text>
+                I wanted to keep this as unopinionated as possible and that is why I didnt create a library but just a collection of examples with minimal abstractions. You can simply install the dependencies and copy the code from the examples to create your own charts.
+            </Text>
             {/* <Heading fontSize="24px" as="h2">Usage</Heading>
             <br />
             <Text>
