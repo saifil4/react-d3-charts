790e292440e27bf4f34ab4336d3a0e180244aedd
saifil <saifilmomin4@gmail.com>
2024-07-07 11:11:29 -0400

    multi line chart data adjustments

--- src/sections/section-list/line-chart-list.tsx
+++ src/sections/section-list/line-chart-list.tsx
@@ -1,14 +1,14 @@
 import { Group } from "types/section.type";
-import * as TChartRaw from "react-d3/data/type?raw";
+import * as TChartAttributes from "react-d3/type?raw";
 import * as AxisRaw from "react-d3/components/Axis?raw";
 
 import LineChart from "react-d3/charts/line-chart/LineChart";
 import * as LineChartRaw from "react-d3/charts/line-chart/LineChart?raw";
 import * as LineChartDataRaw from "react-d3/data/line-chart-data?raw";
 
-import MultiLineChart from "react-d3/charts/line-chart/multi-line-chart";
-import * as MultiLineChartRaw from "react-d3/charts/line-chart/multi-line-chart?raw";
-import * as MultiLineChartConfig from "react-d3/charts/line-chart/multi-line-chart/config?raw";
+import MultiLineChart from "react-d3/charts/line-chart/MultiLineChart";
+import * as MultiLineChartRaw from "react-d3/charts/line-chart/MultiLineChart?raw";
+import * as MultiLineChartData from "react-d3/data/multi-line-chart-data?raw";
 
 
 const lineChartGroup: Group = {
@@ -21,7 +21,7 @@ const lineChartGroup: Group = {
                 { code: LineChartRaw.default, name: "LineChart.tsx" },
                 { code: AxisRaw.default, name: "Axis.tsx" },
                 { code: LineChartDataRaw.default, name: "line-chart-data.ts" },
-                { code: TChartRaw.default, name: "type.ts" },
+                { code: TChartAttributes.default, name: "type.ts" },
             ],
             status: "available"
         },
@@ -30,7 +30,7 @@ const lineChartGroup: Group = {
             component: MultiLineChart,
             files: [
                 { code: MultiLineChartRaw.default, name: "MultiLineChart.tsx" },
-                { code: MultiLineChartConfig.default, name: "config.ts" },
+                { code: MultiLineChartData.default, name: "config.ts" },
             ],
             status: "available"
         },
