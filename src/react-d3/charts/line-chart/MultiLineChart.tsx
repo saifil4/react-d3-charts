3104bcda9322eb0bdf0509d08318ac0eea2d4e44
saifil <saifilmomin4@gmail.com>
2024-07-10 00:25:13 -0400

    added dummy revenue data

--- src/react-d3/charts/line-chart/MultiLineChart.tsx
+++ src/react-d3/charts/line-chart/MultiLineChart.tsx
@@ -1,54 +1,39 @@
 
 import * as d3 from "d3";
 import { motion } from "framer-motion";
-import { chartData, TPlot } from "react-d3/data/multi-line-chart-data";
+import { revenueData, TRevenuePlot } from "react-d3/data/dummy-revenue-data";
 import { Axis } from "react-d3/components/Axis";
-
-
 
 export default function MultiLineChart() {
 
-    const { height, width, data, margin } = chartData;
+    const { height, width, data, margin, title } = revenueData;
     const outerWidth = width + margin.left + margin.right;
     const outerHeight = height + margin.top + margin.bottom;
     const transform = `translate(${margin.left}px, ${margin.top}px)`;
 
-    const color = d3.scaleOrdinal()
-        .range(['#e74c3c', '#3498db', '#2ecc71', '#f1c40f']);
+    const color = d3.scaleOrdinal().range(['#e74c3c', '#3498db', '#2ecc71', '#f1c40f']);
 
-    const parseTime = d3.timeParse('%Y');
-    const dateFormat = d3.timeFormat("%Y");
+    const parseTime = d3.timeParse("%B");
+    const dateFormat = d3.timeFormat("%b");
 
-    const yDomain = d3.extent(data.map(d => d.plot).flat(), d => d.y) as Iterable<number>;
-    const xDomain = d3.extent(data.map(d => d.plot).flat(), d => parseTime(d.x)) as Iterable<Date>;
+    const yDomain = d3.extent(data.map(d => d.plot).flat(), d => d.value) as Iterable<number>;
+    const xDomain = d3.extent(data.map(d => d.plot).flat(), d => parseTime(d.month)) as Iterable<Date>;
 
     const xAxisScale = d3.scaleTime(xDomain, [0, width]);
     const yAxisScale = d3.scaleLinear(yDomain, [height, 0]);
 
-    const xTicks = xAxisScale.ticks(10)
-    const yTicks = yAxisScale.ticks(10)
-
-    const line = d3.line<TPlot>().x(d => xAxisScale(new Date(d.x))).y(d => yAxisScale(d.y));
+    const line = d3.line<TRevenuePlot>().x(d => xAxisScale(parseTime(d.month) as Date)).y(d => yAxisScale(d.value));
 
     return (
         <>
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-                {
-                    data.map((d, i) => (
-                        <div style={{ display: 'flex', alignItems: 'center' }} key={i}>
-                            <div style={{ height: 12, width: 24, backgroundColor: `${color(d.name)}` }}></div>
-                            <span style={{marginRight: '20px', marginLeft: '5px'}}> {d.name} </span>
-                        </div>
-                    ))
-                }
-            </div>
             <ResponsiveSVG viewBox={`0 0 ${outerWidth} ${outerHeight}`}>
                 <g style={{ transform: transform }}>
+                    <text x={width / 2} dy={-20} y={0} textAnchor="middle" style={{ fontSize: "16px", fontWeight: "bold" }}>{title}</text>
                     <Axis
                         orientation="bottom"
                         width={width}
                         height={height}>
-                        {xTicks.map((d) => (<>
+                        {xAxisScale.ticks().map((d) => (<>
                             <Text dy={15} x={xAxisScale(d)} y={0}> {dateFormat(d)} </Text>
                             <Line y1={0} y2={-height} x1={xAxisScale(d)} x2={xAxisScale(d)} />
                         </>))}
@@ -57,8 +42,8 @@ export default function MultiLineChart() {
                         orientation="left"
                         width={width}
                         height={height}>
-                        {yTicks.map((d) => (<>
-                            <Text dx={-10} x={0} y={yAxisScale(d)}> {d} </Text>
+                        {yAxisScale.ticks().map((d) => (<>
+                            <Text dx={-10} x={0} y={yAxisScale(d)} textAnchor="end" > {d} </Text>
                             <Line y1={yAxisScale(d)} y2={yAxisScale(d)} x1={width} x2={0} />
                         </>))}
                     </Axis>
@@ -82,14 +67,22 @@ export default function MultiLineChart() {
                                         key={i}
                                         fill={`${color(d.name)}90`}
                                         stroke={color(d.name) as string}
-                                        cx={xAxisScale(new Date(c.x))}
-                                        cy={yAxisScale(c.y)} />
+                                        cx={xAxisScale(parseTime(c.month) as Date)}
+                                        cy={yAxisScale(c.value)} />
                                 ))}
                             </>
                         ))}
                     </g>
                 </g>
             </ResponsiveSVG>
+            <div style={{ display: 'flex', justifyContent: 'center', padding: '20px 0' }}>
+                {data.map((d, i) => (
+                    <div style={{ display: 'flex', alignItems: 'center' }} key={i}>
+                        <div style={{ height: 12, width: 24, backgroundColor: `${color(d.name)}` }}></div>
+                        <span style={{ marginRight: '20px', marginLeft: '5px' }}> {d.name} </span>
+                    </div>
+                ))}
+            </div>
         </>
     );
 }
