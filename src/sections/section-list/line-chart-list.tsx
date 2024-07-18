82c4bde07ea75abe7fbb32dc630a5cafaa374bf4
saifil <saifilmomin4@gmail.com>
2024-07-18 17:17:24 -0400

    corrected files for line chart module

--- src/sections/section-list/line-chart-list.tsx
+++ src/sections/section-list/line-chart-list.tsx
@@ -1,5 +1,4 @@
 import { TChartSection } from "types";
-import * as TChartAttributes from "react-d3/type?raw";
 import * as AxisRaw from "react-d3/components/Axis?raw";
 
 import LineChart from "react-d3/charts/line-chart/LineChart";
@@ -8,33 +7,33 @@ import * as StockDataRaw from "react-d3/data/stock-data?raw";
 
 import MultiLineChart from "react-d3/charts/line-chart/MultiLineChart";
 import * as MultiLineChartRaw from "react-d3/charts/line-chart/MultiLineChart?raw";
-
+import * as RevenueDataRaw from "react-d3/data/revenue-historic-data?raw";
+import * as LegendRaw from "react-d3/components/Legends?raw";
 
 const lineChartGroup: TChartSection[] = [
-    {
-        group: 'line-chart',
-        heading: "Line Chart",
-        component: LineChart,
-        files: [
-            { code: LineChartRaw.default, name: "LineChart.tsx" },
-            { code: AxisRaw.default, name: "Axis.tsx" },
-            { code: StockDataRaw.default, name: "line-chart-data.ts" },
-            { code: TChartAttributes.default, name: "type.ts" },
-        ],
-        status: "available"
-    },
-    {
-         group: 'line-chart',
-        heading: "Multi Line Chart",
-        component: MultiLineChart,
-        files: [
-            { code: MultiLineChartRaw.default, name: "MultiLineChart.tsx" },
-            { code: 'MultiLineChartData.default', name: "config.ts" },
-        ],
-        status: "available"
-    },
-]
-
-
+  {
+    group: "line-chart",
+    heading: "Line Chart",
+    component: LineChart,
+    files: [
+      { code: LineChartRaw.default, name: "LineChart.tsx" },
+      { code: AxisRaw.default, name: "Axis.tsx" },
+      { code: StockDataRaw.default, name: "stock-data.ts" },
+    ],
+    status: "available",
+  },
+  {
+    group: "line-chart",
+    heading: "Multi Line Chart",
+    component: MultiLineChart,
+    files: [
+      { code: MultiLineChartRaw.default, name: "MultiLineChart.tsx" },
+      { code: AxisRaw.default, name: "Axis.tsx" },
+      { code: LegendRaw.default, name: "Legend.tsx" },
+      { code: RevenueDataRaw.default, name: "config.ts" },
+    ],
+    status: "available",
+  },
+];
 
 export { lineChartGroup };
